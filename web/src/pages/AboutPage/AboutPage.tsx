8cd73de9b636d7a496a9828485323161be6023d8
claycrews2002 <claycrews2002@gmail.com>
2023-08-03 10:32:04 -0400

    feat: about page and resume

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -1,20 +1,30 @@
-import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
-
 const AboutPage = () => {
   return (
-    <>
-      <MetaTags title="About" description="About page" />
-
-      <h1>AboutPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>about</code>, link to me with `
-        <Link to={routes.about()}>About</Link>`
-      </p>
-    </>
+    <div className="hero 1/2-h-screen bg-base-200 rounded-lg">
+      <div className="hero-content flex-col-reverse lg:flex-row-reverse py-20">
+        <div className="flex flex-col">
+          <a href="JamesCrewsResume.pdf" target="_blank">
+            <img
+              src="resume_img.png"
+              className="max-w-sm w-full h-auto rounded-lg shadow-2xl"
+              alt="James Crews Resume"
+            />
+          </a>
+          <p className="italic ml-2">Click to view my resume!</p>
+        </div>
+        <div className="max-w-md text-center mr-10">
+          <h1 className="text-5xl font-bold">James &apos;Clay&apos; Crews</h1>
+          <p className="py-6">
+            Graduating December 2023 with an Undergraduate in Computer
+            Engineering at The University of South Carolina
+          </p>
+          <div>
+            <p>Contact me!</p>
+            <p className="font-semibold">claycrews2002@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
