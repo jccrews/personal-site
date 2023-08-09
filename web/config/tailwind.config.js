e4512e4fa994ca074db60ea0a9e24cf22779d4c0
claycrews2002 <claycrews2002@gmail.com>
2023-08-09 10:55:56 -0400

    feat: color scheme

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#FF8066',
+          secondary: '#B0A8B9',
+          accent: '#FF8066',
+          neutral: '#2a323c',
+          'base-100': '#D5ECF5',
+          info: '#3abff8',
+          success: '#36d399',
+          warning: '#fbbd23',
+          error: '#f87272',
+        },
+      },
+    ],
+  },
   plugins: [require('daisyui')],
 }
