29e61cca769444927ee538c502ba281f1ce15c9f
claycrews2002 <claycrews2002@gmail.com>
2024-07-30 08:41:28 -0400

    update resume pdf name

--- web/src/components/IntroAndResume/IntroAndResume.tsx
+++ web/src/components/IntroAndResume/IntroAndResume.tsx
@@ -3,7 +3,7 @@ const IntroAndResume = () => {
     <div className="hero 1/2-h-screen bg-base-200 rounded-lg">
       <div className="hero-content flex-col-reverse lg:flex-row-reverse py-20">
         <div className="flex flex-col">
-          <a href="JamesCrewsResume.pdf" target="_blank">
+          <a href="resume.pdf" target="_blank">
             <img
               src="resume_img.png"
               className="max-w-sm w-full h-auto rounded-lg shadow-2xl"
@@ -13,14 +13,14 @@ const IntroAndResume = () => {
           <p className="italic ml-2">Click to view my resume!</p>
         </div>
         <div className="max-w-md text-center mr-10">
-          <h1 className="text-5xl font-bold">James &apos;Clay&apos; Crews</h1>
+          <h1 className="text-5xl font-bold">James (Clay) Crews</h1>
           <p className="py-6">
-            Graduating December 2023 with a Bachelor&apos;s degree in Computer
-            Engineering at The University of South Carolina
+            Graduating May 2025 with an M.S. in Computer Engineering at The
+            University of South Carolina
           </p>
           <div>
-            <p>Contact me!</p>
-            <p className="font-semibold">claycrews2002@gmail.com</p>
+            <p>Contact me at:</p>
+            <p className="font-semibold">j.clayton.crews@gmail.com</p>
           </div>
         </div>
       </div>
--- web/src/layouts/NavLayout/NavLayout.tsx
+++ web/src/layouts/NavLayout/NavLayout.tsx
@@ -1,116 +1,130 @@
-import { Link, useLocation } from '@redwoodjs/router'
+// import { Link, useLocation } from '@redwoodjs/router'
 import { Toaster } from '@redwoodjs/web/toast'
 
-import Navbar from 'src/components/Navbar/Navbar'
+// import Navbar from 'src/components/Navbar/Navbar'
 
 type NavLayoutProps = {
   children?: React.ReactNode
 }
 
-const START_MENU_ITEMS = [
-  {
-    to: '/',
-    label: 'Clay Crews',
-  },
-  {
-    label: 'Projects',
-    submenu: [
-      { to: '/projects/wordification', label: 'Wordification' },
-      { to: '/projects/whats-that-plant', label: `What's That Plant` },
-      { to: '/projects/dkms', label: 'DKMS' },
-    ],
-  },
-  {
-    label: 'Personal Projects',
-    submenu: [{ to: '/personal-projects/henhouse', label: 'Henhouse' }],
-  },
-] as const
+// const START_MENU_ITEMS = [
+//   {
+//     to: '/',
+//     label: 'Clay Crews',
+//   },
+//   {
+//     label: 'Projects',
+//     submenu: [
+//       { to: '/projects/wordification', label: 'Wordification' },
+//       { to: '/projects/whats-that-plant', label: `What's That Plant` },
+//       { to: '/projects/dkms', label: 'DKMS' },
+//     ],
+//   },
+//   {
+//     label: 'Personal Projects',
+//     submenu: [{ to: '/personal-projects/henhouse', label: 'Henhouse' }],
+//   },
+// ] as const
 
-export type MenuItem = {
-  to?: string
-  label: string
-  submenu?: readonly {
-    to: string
-    label: string
-  }[]
-}
+// export type MenuItem = {
+//   to?: string
+//   label: string
+//   submenu?: readonly {
+//     to: string
+//     label: string
+//   }[]
+// }
 
-const NavbarItem = ({ item }: { item: MenuItem }) => {
-  const { pathname } = useLocation()
+// const NavbarItem = ({ item }: { item: MenuItem }) => {
+//   const { pathname } = useLocation()
 
-  return (
-    <>
-      {item.to ? (
-        <li>
-          <Link
-            className={`font-bold normal-case text-lg rounded ${
-              pathname === item.to ? 'bg-accent' : ''
-            }`}
-            to={item.to}
-          >
-            {item.label}
-          </Link>
-        </li>
-      ) : (
-        <li className="pointer-events-none">
-          <h1 className="font-bold normal-case text-lg rounded">
-            {item.label}
-          </h1>
-        </li>
-      )}
+//   return (
+//     <>
+//       {item.to ? (
+//         <li>
+//           <Link
+//             className={`font-bold normal-case text-lg rounded ${
+//               pathname === item.to ? 'bg-accent' : ''
+//             }`}
+//             to={item.to}
+//           >
+//             {item.label}
+//           </Link>
+//         </li>
+//       ) : (
+//         <li className="pointer-events-none">
+//           <h1 className="font-bold normal-case text-lg rounded">
+//             {item.label}
+//           </h1>
+//         </li>
+//       )}
 
-      {item.submenu && (
-        <li className="ml-4">
-          {item.submenu.map((subItem) => (
-            <Link
-              className={`font-semibold normal-case rounded ${
-                pathname === subItem.to ? 'bg-accent' : ''
-              }`}
-              to={subItem.to}
-              key={subItem.to}
-            >
-              {subItem.label}
-            </Link>
-          ))}
-        </li>
-      )}
+//       {item.submenu && (
+//         <li className="ml-4">
+//           {item.submenu.map((subItem) => (
+//             <Link
+//               className={`font-semibold normal-case rounded ${
+//                 pathname === subItem.to ? 'bg-accent' : ''
+//               }`}
+//               to={subItem.to}
+//               key={subItem.to}
+//             >
+//               {subItem.label}
+//             </Link>
+//           ))}
+//         </li>
+//       )}
 
-      <div className="divider"></div>
-    </>
-  )
-}
+//       <div className="divider"></div>
+//     </>
+//   )
+// }
 
 const NavLayout = ({ children }: NavLayoutProps) => {
   return (
     <>
-      <div className="drawer">
-        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content">
-          <div className="flex min-h-screen flex-col">
-            <Toaster />
+      <div className="flex min-h-screen flex-col">
+        <Toaster />
 
-            <header className="bg-base-300">
-              <Navbar />
-            </header>
+        {/* <header className="bg-base-300">
+          <Navbar />
+        </header> */}
 
-            <main className="container mx-auto flex-grow p-4 md:px-8">
-              {children}
-            </main>
-          </div>
-        </div>
-        <div className="drawer-side">
-          {
-            // eslint-disable-next-line jsx-a11y/label-has-associated-control
-            <label htmlFor="my-drawer" className="drawer-overlay"></label>
-          }
-          <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
-            {START_MENU_ITEMS.map((item) => (
-              <NavbarItem item={item} key={item.label} />
-            ))}
-          </ul>
-        </div>
+        <main className="container mx-auto flex-grow p-4 md:px-8">
+          {children}
+        </main>
       </div>
     </>
+
+    // <>
+    //   <div className="drawer">
+    //     <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+    //     <div className="drawer-content">
+    //       <div className="flex min-h-screen flex-col">
+    //         <Toaster />
+
+    //         <header className="bg-base-300">
+    //           <Navbar />
+    //         </header>
+
+    //         <main className="container mx-auto flex-grow p-4 md:px-8">
+    //           {children}
+    //         </main>
+    //       </div>
+    //     </div>
+    //     <div className="drawer-side">
+    //       {
+    //         // eslint-disable-next-line jsx-a11y/label-has-associated-control
+    //         <label htmlFor="my-drawer" className="drawer-overlay"></label>
+    //       }
+    //       <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+    //         {START_MENU_ITEMS.map((item) => (
+    //           <NavbarItem item={item} key={item.label} />
+    //         ))}
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </>
   )
 }
 
