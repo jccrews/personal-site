f591c1b002dcef8edd5ad2729519e045f9eb2d39
claycrews2002 <claycrews2002@gmail.com>
2024-08-11 09:23:29 -0400

    fix: resume pdf embed

--- web/src/components/IntroAndResume/IntroAndResume.tsx
+++ web/src/components/IntroAndResume/IntroAndResume.tsx
@@ -3,7 +3,11 @@ const IntroAndResume = () => {
     <div className="hero 1/2-h-screen bg-base-200 rounded-lg">
       <div className="hero-content flex-col-reverse lg:flex-row-reverse py-20">
         <div className="flex flex-col">
-          <a href="resume.pdf" target="_blank">
+          <a
+            href="https://docs.google.com/viewer?url=https://jccrews.com/resume-file.pdf&embedded=true"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               src="resume_img.png"
               className="max-w-sm w-full h-auto rounded-lg shadow-2xl"
