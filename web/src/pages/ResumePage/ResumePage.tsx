9ed2941e55a6398bc17bceebe76f93c42ef481af
Clay Crews <claycrews2002@gmail.com>
2025-01-10 18:04:42 -0500

    fix - remove resume page and just do pdf

--- web/src/pages/ResumePage/ResumePage.tsx
+++ web/src/pages/ResumePage/ResumePage.tsx
@@ -1,33 +1,25 @@
-// import React from 'react'
-
-// const ResumePage = () => {
-//   return (
-//     <div className="w-full h-screen flex items-center justify-center bg-gray-100">
-//       <div className="w-full h-full max-w-6xl shadow-lg">
-//         <embed
-//           src="/resume.pdf"
-//           type="application/pdf"
-//           width="100%"
-//           height="100%"
-//           className="w-full h-full"
-//           title="James_Crews_Resume"
-//         />
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default ResumePage
-
 import React from 'react'
 
 const ResumePage = () => {
   return (
-      <iframe src="http://docs.google.com/gview?url=http://jccrews.com/resume.pdf&embedded=true"
-        style="width:600px; height:500px;" frameborder="0"></iframe>
 
+    <div className="w-full h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full h-full max-w-6xl shadow-lg">
+        <embed
+          src="/resume.pdf"
+          type="application/pdf"
+          width="100%"
+          height="100%"
+          className="w-full h-full"
+          title="James_Crews_Resume"
+        />
+      </div>
+      <p>DO NOT USE THIS</p>
+    </div>
   )
 }
 
 export default ResumePage
+
+
 
