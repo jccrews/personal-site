876958395c2ac71e3f7c50358cded639c18b8e6c
Clay Crews <claycrews2002@gmail.com>
2025-01-10 17:51:05 -0500

    fix attempt

--- web/src/pages/ResumePage/ResumePage.tsx
+++ web/src/pages/ResumePage/ResumePage.tsx
@@ -18,57 +18,16 @@
 // }
 
 // export default ResumePage
-import React, { useState, useEffect } from 'react'
+
+import React from 'react'
 
 const ResumePage = () => {
-  const [pdfError, setPdfError] = useState(false)
-  const pdfUrl = '/resume.pdf' // Adjust this path as needed
-
-  useEffect(() => {
-    // Check if the PDF can be loaded
-    fetch(pdfUrl)
-      .then((response) => {
-        if (!response.ok) {
-          setPdfError(true)
-        }
-      })
-      .catch(() => setPdfError(true))
-  }, [])
-
-  if (pdfError) {
-    return (
-      <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-100">
-        <p className="text-xl mb-4">Unable to display the PDF.</p>
-        <a
-          href={pdfUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Download Resume PDF
-        </a>
-      </div>
-    )
-  }
-
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-gray-100">
-      <div className="w-full h-full max-w-6xl shadow-lg">
-        <object
-          data={pdfUrl}
-          type="application/pdf"
-          width="100%"
-          height="100%"
-          className="w-full h-full"
-        >
-          <p>
-            Your browser does not support PDFs.
-            <a href={pdfUrl}>Download the PDF</a>.
-          </p>
-        </object>
-      </div>
-    </div>
+      <iframe src="http://docs.google.com/gview?url=http://jccrews.com/resume.pdf&embedded=true"
+        style="width:600px; height:500px;" frameborder="0"></iframe>
+
   )
 }
 
 export default ResumePage
+
